e269410f0aa780643a4f14684fda38dde2f17d24
minimi61 <new1573@naver.com>
2022-12-09 19:27:55 +0900

    [test] json data pull

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from './components/Button';
+import data from './data/1bus/weekday.json'
 
 export default function App() {
+  const time = Date.now()
+  const dd = new Date(data[1][2].split(':'))
+  console.log(dd.getMinutes())
+
+ 
+  //{data.map((x, idx) =>       console.log(x) ) }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}>   
+     
       <StatusBar style="auto" />
       <Button/>
     </View>
